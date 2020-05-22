f43ccdd5dfdae61e07244f6b5034cb7677d49fc9
polskipepek <lonely.navel@gmail.com>
2020-05-22 23:43:03 +0200

    remerge

--- Logic/Seeders/Fake/CustomersFakeSeeds.cs
+++ Logic/Seeders/Fake/CustomersFakeSeeds.cs
@@ -6,7 +6,7 @@ namespace Logic.Seeders.Fake {
     class CustomersFakeSeeds : SeedBase {
         public override void Seed (Context context) {
             var tempCustomers = new List<Customer> ();
-            for (int t = 0; t < 20; t++) {
+            for (int t = 0; t < 1; t++) {
                 tempCustomers.Add (new Customer {
                     CustomerName = Faker.Generators.Names.First (),
                     CustomerSurname = Faker.Generators.Names.Last ()
